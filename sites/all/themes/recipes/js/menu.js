eda78b516b544a45b547580985c199386beb4851
Brad Allen Fisher <bradallenfisher>
2015-07-12 17:50:54 +0000

    first commit

---
+++ sites/all/themes/recipes/js/menu.js
@@ -0,0 +1,21 @@
+(function ($) {
+
+  'use strict';
+   $( document ).ready(function() {
+
+    $('#edit-field-category-value-wrapper label').click (
+	    function() {
+				$('.form-item-field-category-value').toggleClass('open');
+				$('.form-item-field-food-type-value').removeClass('open');
+			}
+    );
+
+    $('#edit-field-food-type-value-wrapper label').click (
+	    function() {
+				$('.form-item-field-food-type-value').toggleClass('open');
+				$('.form-item-field-category-value').removeClass('open');
+			}
+    );
+
+  });
+})(jQuery);
---
+++ sites/all/themes/recipes/templates/page.tpl.php
@@ -0,0 +1,29 @@
+<div class="page">
+  <?php print $messages; ?>
+  <?php print render($tabs); ?>
+
+  <div class="main">
+    <div class="content" role="main">
+      <div class="header">
+      <a href="/">NUTRIMOST<span>RECIPES</span></a>
+    </div>
+
+    <div class="menu">
+      <?php print render($page['menu']);?>
+    </div>
+      <a id="main-content"></a>
+      <?php if(!$is_front):?>
+        <h1><?php print $title; ?></h1>
+      <?php endif;?>
+      <?php print render($page['content']); ?>
+    </div>
+    <div class="side">
+      <?php print render($page['side']);?>
+    </div>
+  </div>
+
+  <footer class="footer" role="contentinfo">
+    <?php print render($page['footer']); ?>
+  </footer>
+</div>
+
